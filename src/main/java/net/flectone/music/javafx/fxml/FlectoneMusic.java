448d7bcaaaedb5b834546b195eb9b8d1ea27e66a
TheFaser <artem.mochalin11@gmail.com>
2024-04-07 19:20:18 +0500

    add icon

--- src/main/java/net/flectone/music/javafx/fxml/FlectoneMusic.java
+++ src/main/java/net/flectone/music/javafx/fxml/FlectoneMusic.java
@@ -3,6 +3,7 @@ package net.flectone.music.javafx.fxml;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import lombok.Getter;
 import net.flectone.music.Main;
@@ -19,8 +20,12 @@ public class FlectoneMusic extends Application {
 
     private Stage twitchStage;
 
+    private static Image icon;
+
     @Override
     public void start(Stage stage) throws IOException {
+        icon = new Image("net/flectone/music/image/flectone.png");
+
         showAppScene(stage);
         stage.setOnCloseRequest(e -> stop());
     }
@@ -36,6 +41,7 @@ public class FlectoneMusic extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("app.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         stage.setTitle("FlectoneMusic - музыка стримера и его зрителей");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
 
@@ -52,6 +58,7 @@ public class FlectoneMusic extends Application {
             twitchStage.setTitle("Настройка Twitch");
             twitchStage.setScene(scene);
             twitchStage.setAlwaysOnTop(true);
+            twitchStage.getIcons().add(icon);
         }
 
         if (twitchStage.isShowing()) return;
