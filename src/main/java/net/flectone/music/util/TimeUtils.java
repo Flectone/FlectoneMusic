a788b91c1b21e63cd584f3f273b9aa49b218a441
TheFaser <artem.mochalin11@gmail.com>
2024-04-07 15:33:07 +0500

    fix time parse

--- src/main/java/net/flectone/music/util/TimeUtils.java
+++ src/main/java/net/flectone/music/util/TimeUtils.java
@@ -7,7 +7,7 @@ public class TimeUtils {
 
         int time = 0;
         for (int x = 0; x < stringTime.length; x++) {
-            time += (int) (Integer.parseInt(stringTime[x]) * Math.pow(60, stringTime.length - x));
+            time += (int) (Integer.parseInt(stringTime[x]) * Math.pow(60, stringTime.length - x - 1));
         }
 
         return time;
